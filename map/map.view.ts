12c42644632f394d8aaa1453d029cd1e095179dd
Pavel Zubkov <zubkov_p@mail.ru>
2023-10-01 01:02:24 +0300

    Position watching now

--- map/map.view.ts
+++ map/map.view.ts
@@ -1,4 +1,68 @@
 namespace $.$$ {
+
+	class Position extends $mol_object {
+
+		options() {
+			return { enableHighAccuracy: true }
+		}
+
+		@ $mol_mem
+		watcher() {
+			const id = this.$.$mol_dom_context.navigator.geolocation.watchPosition(
+				$mol_wire_async( (val: GeolocationPosition) => this.value(val) ),
+				$mol_wire_async( (error: GeolocationPositionError) => this.error(error) ),
+				this.options(),	
+			)
+
+			return { destructor: () => this.$.$mol_dom_context.navigator.geolocation.clearWatch(id) }
+		}
+
+		@ $mol_mem
+		value(next?: GeolocationPosition) {
+			this.watcher()
+			return next ?? null
+		}
+
+		@ $mol_mem
+		error(next?: GeolocationPositionError) {
+			this.watcher()
+			return next ?? null
+		}
+
+		accuracy() {
+			return this.value()?.coords?.accuracy
+		}
+
+		altitude() {
+			return this.value()?.coords?.altitude
+		}
+
+		altitudeAccuracy() {
+			return this.value()?.coords?.altitudeAccuracy
+		}
+
+		heading() {
+			return this.value()?.coords?.heading
+		}
+
+		latitude() {
+			return this.value()?.coords?.latitude
+		}
+
+		longitude() {
+			return this.value()?.coords?.longitude
+		}
+
+		speed() {
+			return this.value()?.coords?.speed
+		}
+
+		timestamp() {
+			return this.value()?.timestamp
+		}
+		
+	}
+
 	export class $hype_escape_map extends $.$hype_escape_map {
 
 		@ $mol_memo.field
@@ -8,31 +72,19 @@ namespace $.$$ {
           })
         }
 		
-		getCurrentPositioin(): Promise<GeolocationPosition> {
-			return new Promise<GeolocationPosition>((res, rej) => {
-				this.$.$mol_dom_context.navigator.geolocation.getCurrentPosition(res, rej, { enableHighAccuracy: true })
-			})
+		@ $mol_mem
+		position() {
+			return new Position
 		}
 
 		@ $mol_mem
 		position_x() {
-			return this.position().coords.longitude
+			return this.position().longitude() ?? 0
 		}
 
 		@ $mol_mem
 		position_y() {
-			return this.position().coords.latitude
-		}
-
-		position() {
-			const res = $mol_wire_sync( this ).getCurrentPositioin() as GeolocationPosition
-			console.log(res)
-			return res
-		}
-
-		@ $mol_mem
-		position_now(next?: GeolocationPosition) {
-			return next
+			return this.position().latitude() ?? 0
 		}
 		
 	}
