3978120ba1c7f07786dc89445ed9a81d47797733
PavelZubkov <PavelZubkov@users.noreply.github.com>
2024-12-18 07:10:28 +0000

    Deploying to gh-pages from @ PavelZubkov/hype_escape@12c42644632f394d8aaa1453d029cd1e095179dd 🚀

--- node.js
+++ node.js
@@ -3770,7 +3770,8 @@ var $;
                     stabilityThreshold: 100,
                 },
             });
-            watcher.on('all', (type, path) => {
+            watcher
+                .on('all', (type, path) => {
                 if (path instanceof Error) {
                     this.$.$mol_fail_log(path);
                     return;
@@ -3783,7 +3784,8 @@ var $;
                 else {
                     file.parent().reset();
                 }
-            });
+            })
+                .on('error', $mol_fail_log);
             return {
                 destructor() {
                     watcher.close();
--- node.test.js
+++ node.test.js
@@ -3761,7 +3761,8 @@ var $;
                     stabilityThreshold: 100,
                 },
             });
-            watcher.on('all', (type, path) => {
+            watcher
+                .on('all', (type, path) => {
                 if (path instanceof Error) {
                     this.$.$mol_fail_log(path);
                     return;
@@ -3774,7 +3775,8 @@ var $;
                 else {
                     file.parent().reset();
                 }
-            });
+            })
+                .on('error', $mol_fail_log);
             return {
                 destructor() {
                     watcher.close();
