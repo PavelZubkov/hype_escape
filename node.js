a7abd15021021713c38ec7d3d17ade18e5c6ecaf
PavelZubkov <PavelZubkov@users.noreply.github.com>
2025-01-01 07:10:03 +0000

    Deploying to gh-pages from @ PavelZubkov/hype_escape@12c42644632f394d8aaa1453d029cd1e095179dd 🚀

--- node.js
+++ node.js
@@ -8947,7 +8947,6 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
-            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
